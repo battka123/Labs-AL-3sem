10d5db9ecbf2ab66245c909a92b712a007e7af81
Ivan <55855887+battka123@users.noreply.github.com>
2023-12-29 12:02:45 +0300

    :)

---
+++ Lab07/Program.cs
@@ -0,0 +1,82 @@
+﻿using System.Reflection;
+using System.Xml;
+using AnimalClasses;
+using AnimalClasses.classes;
+using AnimalClasses.abstract_classes;
+using AnimalClasses.enums;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlDocument xmlDoc = new XmlDocument();
+
+        XmlElement rootElement = xmlDoc.CreateElement("ClassDiagram");
+        xmlDoc.AppendChild(rootElement);
+
+        // get all types from AnimalClasses assembly
+        Assembly assembly = Assembly.Load("AnimalClasses");
+        Type[] types = assembly.GetTypes();
+
+        foreach (Type type in types)
+        {
+            // class-enum difference
+            string t;
+            if (type.IsClass)
+            {
+                t = "Class";
+            }
+            else
+            {
+                t = "Enum";
+            }
+
+            // take to xml classes and enums created in AnimalClasses
+            if (type.Namespace.Contains("AnimalClasses"))
+            {
+                XmlElement element = xmlDoc.CreateElement(t);
+                rootElement.AppendChild(element);
+
+                // add name attribute
+                element.SetAttribute("name", type.Name);
+
+                // get comment attribute
+                CommentAttribute comment = (CommentAttribute)type.GetCustomAttribute(typeof(CommentAttribute));
+
+                if (comment != null)
+                {
+                    // element for comment
+                    XmlElement commentElement = xmlDoc.CreateElement("Comment");
+                    commentElement.InnerText = comment.Comment;
+                    element.AppendChild(commentElement);
+                }
+
+                // get all properties and put in xml
+                object[] properties = type.GetProperties();
+
+                foreach (var prop in properties)
+                {
+                    XmlElement propertyElement = xmlDoc.CreateElement("Property");
+                    propertyElement.InnerText = prop.ToString();
+                    element.AppendChild(propertyElement);
+                }
+
+                //get all methods ant put in xml
+                object[] methods = type.GetMethods(BindingFlags.DeclaredOnly);
+
+                foreach (var method in methods)
+                {
+                    XmlElement methodElement = xmlDoc.CreateElement("Method");
+                    methodElement.InnerText = method.ToString();
+                    element.AppendChild(methodElement);
+                }
+            }
+
+        }
+
+        // save xml-document
+        xmlDoc.Save("C:\\C#\\Lab07\\ClassDiagram.xml");
+
+    }
+}
---
+++ Lab07/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Lab07")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab07")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("936ac697-526a-419e-8c6f-ab0c3cfdde6e")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

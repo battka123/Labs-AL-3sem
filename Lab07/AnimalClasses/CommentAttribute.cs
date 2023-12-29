c16756960eb953ed37767d9062280e530fec285b
Ivan <55855887+battka123@users.noreply.github.com>
2023-12-29 13:15:04 +0300

    :)

---
+++ Lab07/AnimalClasses/CommentAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AnimalClasses
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum)]
+    public class CommentAttribute : Attribute
+    {
+        public string Comment { get; }
+
+        public CommentAttribute(string comment)
+        {
+            Comment = comment;
+        }
+    }
+}
---
+++ Lab07/AnimalClasses/classes/Pig.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using AnimalClasses.abstract_classes;
+using AnimalClasses.enums;
+
+namespace AnimalClasses.classes
+{
+    [Comment("Class for pig")]
+    class Pig : Animal
+    {
+        public override eFavoriteFood GetFavoriteFood()
+        {
+            return eFavoriteFood.Everything;
+        }
+
+        public override void SayHello()
+        {
+            Console.WriteLine("OOINK");
+        }
+    }
+}

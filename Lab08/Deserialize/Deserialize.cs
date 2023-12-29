7381b19f109bb301cef79e8195a878a6b23ab8db
Ivan <55855887+battka123@users.noreply.github.com>
2023-12-29 12:03:23 +0300

    :)

---
+++ Lab08/Deserialize/Deserialize.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using System;
+using System.Xml.Serialization;
+using AnimalClasses.abstract_classes;
+using AnimalClasses.classes;
+/*
+class Program
+{
+    static void Main()
+    {
+        XmlSerializer xmlSerializer = new XmlSerializer(typeof(Cow));
+
+        using (FileStream fs = new FileStream("C:\\C#\\Lab08\\animal.xml", FileMode.OpenOrCreate))
+        {
+            Animal cow = xmlSerializer.Deserialize(fs) as Cow;
+            Console.WriteLine($"Country: {cow?.Country}\nHide from other animals: {cow?.HideFromOtherAnimals}\n" +
+                              $"Name: {cow?.Name}\nClassification: {cow?.GetClassificationAnimal}");
+            Console.ReadLine();
+        }
+    }
+}
+*/
---
+++ Lab08/Serialize/Serialize.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System;
+using System.Xml.Serialization;
+using AnimalClasses.abstract_classes;
+using AnimalClasses.classes;
+using AnimalClasses.enums;
+/*
+namespace Serialize
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // create Animal example
+            Animal cow = new Cow
+            {
+                Country = "Russia",
+                HideFromOtherAnimals = false,
+                Name = "Murka",
+                GetClassificationAnimal = eClassificationAnimal.Herbivores
+            };
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Cow));
+
+            // get stream and serialize object
+            using (FileStream fs = new FileStream("C:\\C#\\Lab08\\animal.xml", FileMode.OpenOrCreate))
+            {
+                xmlSerializer.Serialize(fs, cow);
+                Console.WriteLine("Serialized");
+            }
+        }
+    }
+}
+*/
